6c3af1713383cbd34ff450df83c15688442a2445
Mohammad-AbuJaber <130397610+Mohammad-AbuJaber@users.noreply.github.com>
2023-06-20 02:28:24 +0300

    Update get_input_args.py (adding forward slash for --dir)

--- Pre-trained Image Classifier to Identify Dog Breeds/get_input_args.py
+++ Pre-trained Image Classifier to Identify Dog Breeds/get_input_args.py
@@ -22,32 +22,34 @@ import argparse
 #       collection that you created with this function
 # 
 def get_input_args():
-    """
-    Retrieves and parses the 3 command line arguments provided by the user when
-    they run the program from a terminal window. This function uses Python's 
-    argparse module to created and defined these 3 command line arguments. If 
-    the user fails to provide some or all of the 3 arguments, then the default 
-    values are used for the missing arguments. 
-    Command Line Arguments:
-      1. Image Folder as --dir with default value 'pet_images'
-      2. CNN Model Architecture as --arch with default value 'vgg'
-      3. Text File with Dog Names as --dogfile with default value 'dognames.txt'
-    This function returns these arguments as an ArgumentParser object.
-    Parameters:
-     None - simply using argparse module to create & store command line arguments
-    Returns:
-     parse_args() -data structure that stores the command line arguments object  
-    """
-    # Create Parse using ArgumentParser
-    parser = argparse.ArgumentParser(description='Classify pet images')
+  """
+  Retrieves and parses the 3 command line arguments provided by the user when
+  they run the program from a terminal window. This function uses Python's 
+  argparse module to create and define these 3 command line arguments. If 
+  the user fails to provide some or all of the 3 arguments, then the default 
+  values are used for the missing arguments. 
+  Command Line Arguments:
+    1. Image Folder as --dir with default value 'pet_images'
+    2. CNN Model Architecture as --arch with default value 'vgg'
+    3. Text File with Dog Names as --dogfile with default value 'dognames.txt'
+  This function returns these arguments as an ArgumentParser object.
+  Parameters:
+    None - simply using argparse module to create & store command line arguments
+  Returns:
+    parse_args() - data structure that stores the command line arguments object  
+  """
+  # Create a parser using ArgumentParser
+  parser = argparse.ArgumentParser(description='Classify pet images')
 
-    
-    # Create 3 command line arguments as mentioned above using add_argument() from ArguementParser method
-    parser.add_argument('--dir', type=str, default='pet_images', help='directory with images')
-    parser.add_argument('--arch', type=str, default='vgg', help='model architecture')
-    parser.add_argument('--dogfile', type=str, default='dognames.txt', help='file that contains dognames')
-    
-    
-    # Replace None with parser.parse_args() parsed argument collection that 
-    # you created with this function 
-    return parser.parse_args()
+  # Create 3 command line arguments as mentioned above using add_argument() from ArgumentParser method
+  # the notes were taken into consideration, the forward slash was added and more descriptive help messages to each CLI argument was added
+  # for the user: in order to see the help message: run the program with -help or -h at the end of the run command
+  # I've added the metavar='image_folder' parameter to the --dir argument. This sets the name of the argument to be displayed in the usage messages as image_folder
+  # This can help users understand what value is expected for that argument
+  parser.add_argument('--dir', type=str, default='pet_images/', metavar='image_folder', help='path to the folder that contains pet images')
+  parser.add_argument('--arch', type=str, default='vgg', help='CNN model architecture')
+  parser.add_argument('--dogfile', type=str, default='dognames.txt', help='path to the file that contains dog names')
+
+  # Replace None with parser.parse_args() parsed argument collection that 
+  # you created with this function 
+  return parser.parse_args()
